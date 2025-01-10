18975730254e12e1ca9ba29a3d3bcc3fd9df199e
kausik <kkausik11@gmail.com>
2025-01-10 11:08:14 +0530

    last commit

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,22 +1,46 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "",
+//   authDomain: "",
+//   projectId: "",
+//   storageBucket: "",
+//   messagingSenderId: "",
+//   appId: "",
+//   measurementId: "",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = getAuth(app);
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase con    figuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: "",
+  apiKey: "AIzaSyAf32ivyBY2jSYyVHSea5puWTTO6a5Zao8",
+  authDomain: "otp-authentication-59684.firebaseapp.com",
+  projectId: "otp-authentication-59684",
+  storageBucket: "otp-authentication-59684.appspot.com",
+  messagingSenderId: "750681396918",
+  appId: "1:750681396918:web:3a23f2d4a635182f817817",
+  measurementId: "G-50JDXQNWJ2"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
